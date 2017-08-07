cabdf8dc9c4c99f9e32e972820d65e3a0dbf924b
HendrikVE <hendrik1995@msn.com>
2017-08-07 14:41:50 +0200

    init files

---
+++ host/install_host.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+if [ "$(whoami)" = "root" ]; then
+  TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
+else
+  TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
+fi
+
+HOST_NAME=de.fu_berlin.mi.riot_app_market
+
+# Create directory to store native messaging host.
+mkdir -p "$TARGET_DIR"
+
+# Copy native messaging host manifest.
+cp "$DIR/$HOST_NAME.json" "$TARGET_DIR"
+
+# Update host path in the manifest.
+HOST_PATH=$DIR/riot_app_market
+ESCAPED_HOST_PATH=${HOST_PATH////\\/}
+sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
+
+# Set permissions for the manifest so that all users can read it.
+chmod o+r "$TARGET_DIR/$HOST_NAME.json"
+
+echo "Native messaging host $HOST_NAME has been installed."
