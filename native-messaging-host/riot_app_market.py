1cc5740f767338e050e59ce9141898e3d1bbf6da
HendrikVE <hendrik1995@msn.com>
2017-08-28 00:19:15 +0200

    saving copy of built application in to /tmp/riotam/

--- native-messaging-host/riot_app_market.py
+++ native-messaging-host/riot_app_market.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python
-import time, subprocess, sys, json, struct
+import os, errno, time, subprocess, sys, json, struct
+import json, base64
 
 def main(cmd):
     
-    proc = subprocess.Popen(["gnome-terminal -e './flash pba-d-01-kw2x'"], shell=True)
+    json_message = json.loads(get_message())
+    output_file_content = base64.b64decode(json_message["output_file"])
+    device = json_message["device"]
+    application_name = json_message["application_name"]
+    
+    try:
+        create_directories("/tmp/riotam/")
+        
+        with open("/tmp/riotam/" + application_name + ".elf", "wb") as executable:
+            executable.write(output_file_content)
+            
+    except Exception as e:
+        print e
+    
+    proc = subprocess.Popen(["gnome-terminal -e './flash " + device + "'"], shell=True)
     #build_result["cmd_output"] += proc.communicate()[0].replace("\n", "<br>")
-   
+    
+def create_directories(path):
+    
+    try:
+        os.makedirs(path)
+
+    except OSError as e:
+
+        if e.errno != errno.EEXIST:
+            raise
+    
+# https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/nativeMessaging/host/native-messaging-example-host
 def get_message():
     
     # Read the message length (first 4 bytes).
