ada74d6a28edfca3c97529306d5047f03677df1d
HendrikVE <hendrik1995@msn.com>
2017-09-26 01:47:11 +0200

    added possible request to check connection to native messaging host

--- extension/eventPage.js
+++ extension/eventPage.js
@@ -1,8 +1,9 @@
 
-var hostName = "de.fu_berlin.mi.riot_app_market";
-
 chrome.runtime.onMessageExternal.addListener(
+
     function(request, sender, sendResponse) {
+
+        var hostName = "de.fu_berlin.mi.riot_app_market";
 
         if (request.request != null) {
 
@@ -10,20 +11,40 @@ chrome.runtime.onMessageExternal.addListener(
                 var manifest = chrome.runtime.getManifest();
                 sendResponse({version: manifest.version});
             }
+            else if (request.request == "native_messaging_host_accessible") {
+                chrome.runtime.sendNativeMessage(hostName, {native_messaging_host_accessible: true},
+                    function() {
+                        if (chrome.runtime.lastError) {
+                            if (chrome.runtime.lastError.message == "Specified native messaging host not found.") {
+
+                                sendResponse({success: false});
+                                return;
+                            }
+                        }
+
+                        sendResponse({success: true});
+                    }
+                );
+                // send a response asynchronously. See: https://developer.chrome.com/apps/runtime#event-onMessageExternal
+                return true;
+            }
             else {
                 sendResponse({error: "unknown request type: " + request.request});
             }
         }
-        
-        //if output_file is null, something did not work when building the application
-        if (request.output_file != null) {
-            chrome.runtime.sendNativeMessage(hostName, request,
-                function() {
-                    if (chrome.runtime.lastError.message == "Specified native messaging host not found.") {
-                        alert("You need to install the riotam Native Messaging Host provided in riotam-chrome-integration/native-messaging-host/");
+        else {
+            //if output_file is null, something did not work when building the application
+            if (request.output_file != null) {
+                chrome.runtime.sendNativeMessage(hostName, request,
+                    function() {
+                        if (chrome.runtime.lastError) {
+                            if (chrome.runtime.lastError.message == "Specified native messaging host not found.") {
+                                alert("You need to install the riotam Native Messaging Host provided in riotam-chrome-integration/native-messaging-host/");
+                            }
+                        }
                     }
-                }
-            );
+                );
+            }
         }
     }
 );
--- native-messaging-host/riot_app_market.py
+++ native-messaging-host/riot_app_market.py
@@ -15,47 +15,54 @@ from subprocess import Popen
 
 
 def main():
-    
+
     json_message = json.loads(get_message())
-    
+
+    try:
+        # just testing connectivity
+        if json_message["native_messaging_host_accessible"]:
+            return
+    except:
+        pass
+
     output_file_content = base64.b64decode(json_message["output_file"])
     output_file_extension = json_message["output_file_extension"]
-    
+
     output_archive_content = base64.b64decode(json_message["output_archive"])
     output_archive_extension = json_message["output_archive_extension"]
 
     board = json_message["board"]
     application_name = json_message["application_name"]
-    
+
     try:
         temporary_directory = "tmp/" + application_name + "/"
         create_directories(temporary_directory)
-        
+
         archive_file_path = temporary_directory + application_name + "." + output_archive_extension
-        
+
         with open(archive_file_path, "wb") as archive:
             archive.write(output_archive_content)
-            
+
         dest_path = temporary_directory + application_name + "/"
         create_directories(dest_path)
-        
+
         tar = tarfile.open(archive_file_path, "r:gz")
         for tarinfo in tar:
             tar.extract(tarinfo, dest_path)
-            
+
         tar.close()
 
         path_to_makefile = os.path.join(dest_path, "generated_by_riotam", application_name)
         Popen(["x-terminal-emulator", "-e", "./flash {0} {1}".format(board, path_to_makefile)])
 
         #rmtree(temporary_directory)
-        
+
     except Exception as e:
         logging.error(str(e), exc_info=True)
 
 
 def create_directories(path):
-    
+
     try:
         os.makedirs(path)
 
@@ -67,7 +74,7 @@ def create_directories(path):
 
 # https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/nativeMessaging/host/native-messaging-example-host
 def get_message():
-    
+
     # Read the message length (first 4 bytes).
     text_length_bytes = sys.stdin.read(4)
     if len(text_length_bytes) == 0:
@@ -78,7 +85,7 @@ def get_message():
 
     # Read the text (JSON object) of the message.
     text = sys.stdin.read(text_length).decode('utf-8')
-    
+
     return text
 
 
