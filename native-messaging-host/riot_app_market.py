bdbe41dfbce6206fbf9c88054442983177690895
HendrikVE <hendrik1995@msn.com>
2017-12-15 11:24:56 +0100

    deleting mechanism for native messaging hosts temporary files

--- native-messaging-host/riot_app_market.py
+++ native-messaging-host/riot_app_market.py
@@ -18,9 +18,13 @@ import os
 import struct
 import sys
 import tarfile
+import time
+from shutil import rmtree
 from subprocess import Popen
 
 import common
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def main():
@@ -29,42 +33,51 @@ def main():
 
     try:
         # just testing connectivity
-        if json_message["native_messaging_host_accessible"]:
+        if json_message['native_messaging_host_accessible']:
             return
 
     except Exception as e:
         pass
 
-    output_archive_content = base64.b64decode(json_message["output_archive"])
-    output_archive_extension = json_message["output_archive_extension"]
+    output_archive_content = base64.b64decode(json_message['output_archive'])
+    output_archive_extension = json_message['output_archive_extension']
 
-    board = json_message["board"]
-    application_name = json_message["application_name"]
+    board = json_message['board']
+    application_name = json_message['application_name']
 
     try:
-        temporary_directory = os.path.join("tmp", application_name)
+        temporary_directory = os.path.join('tmp', application_name)
         common.create_directories(temporary_directory)
 
-        archive_file_path = os.path.join(temporary_directory, application_name + "." + output_archive_extension)
+        archive_file_path = os.path.join(temporary_directory, application_name + '.' + output_archive_extension)
 
-        with open(archive_file_path, "wb") as archive:
+        with open(archive_file_path, 'wb') as archive:
             archive.write(output_archive_content)
 
         dest_path = os.path.join(temporary_directory, application_name)
         common.create_directories(dest_path)
 
-        tar = tarfile.open(archive_file_path, "r:gz")
+        tar = tarfile.open(archive_file_path, 'r:gz')
         for tarinfo in tar:
             tar.extract(tarinfo, dest_path)
 
         tar.close()
 
-        path_to_makefile = os.path.join(dest_path, "generated_by_riotam", application_name)
+        path_to_makefile = os.path.join(dest_path, 'generated_by_riotam', application_name)
 
-        #open standard terminal and execute shell script "flash"
-        Popen(["x-terminal-emulator", "-e", "./flash {0} {1}".format(board, path_to_makefile)])
+        #open standard terminal and execute shell script 'flash'
+        file_to_look_at = os.path.join(CUR_DIR, '.terminal_finished_for_' + application_name)
 
-        #rmtree(temporary_directory)
+        logging.debug(file_to_look_at)
+
+        inner_command = './flash {0} {1} && touch {2}'.format(board, path_to_makefile, file_to_look_at.replace(' ', '\ '))
+        Popen(['x-terminal-emulator', '-e', 'bash -c "{}"'.format(inner_command)])
+
+        while not os.path.exists(file_to_look_at):
+            time.sleep(1)
+
+        os.remove(file_to_look_at)
+        rmtree(temporary_directory)
 
     except Exception as e:
         logging.error(str(e), exc_info=True)
@@ -87,17 +100,17 @@ def get_message_from_stdin():
         sys.exit(0)
 
     # Unpack message length as 4 byte integer.
-    text_length = struct.unpack("i", text_length_bytes)[0]
+    text_length = struct.unpack('i', text_length_bytes)[0]
 
     # Read the text (JSON object) of the message.
-    text = sys.stdin.read(text_length).decode("utf-8")
+    text = sys.stdin.read(text_length).decode('utf-8')
 
     return text
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    logging.basicConfig(filename="log/riot_app_market_log.txt", format="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.DEBUG)
+    logging.basicConfig(filename='log/riot_app_market_log.txt', format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 
     try:
         main()
